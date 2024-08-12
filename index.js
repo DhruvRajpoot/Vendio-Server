44566960cb8e4ca9c28df9df2a29d1dcadcf4ba1
Dhruv Rajpoot <dhruv.kutarya@gmail.com>
2024-08-12 15:21:21 +0530

    feat: database models

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import connectdatabase from "./Database/db.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+connectdatabase();
+
+app.listen(port, () => {
+  console.log(`listening at port ${port}`);
+});
