93236810a158b4c8115a17b405cca77914a20a7a
Dhruv Rajpoot <dhruv.kutarya@gmail.com>
2024-08-16 03:49:02 +0530

    feat: wishlist

--- index.js
+++ index.js
@@ -20,10 +20,12 @@ connectdatabase();
 import authRoutes from "./routes/auth.js";
 import cartRoutes from "./routes/cart.js";
 import orderRoutes from "./routes/order.js";
+import wishlistRoutes from "./routes/wishlist.js";
 
 app.use("/api/auth", authRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/order", orderRoutes);
+app.use("/api/wishlist", wishlistRoutes);
 
 app.listen(port, () => {
   console.log(`listening at port ${port}`);
