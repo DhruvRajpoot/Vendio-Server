6c7fe3b42b6e7d766691baff9e44762877638d8a
Dhruv Rajpoot <dhruv.kutarya@gmail.com>
2024-09-11 00:17:04 +0530

    feat: userdetails

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,40 @@
 import { deleteImageOnCloudinary } from "../utils/cloudinary.js";
+import User from "../database/models/user.js";
+
+export const getUserDetails = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const user = await User.findById(userId).select(
+      "-password -createdAt -updatedAt -__v"
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({
+      message: "User details fetched successfully",
+      user: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+      message: "Error while fetching user details",
+    });
+  }
+};
 
 export const updateProfilePic = async (req, res) => {
   try {
-    const user = req.user;
+    const userId = req.user._id;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found " });
+    }
+
     const { pic, oldPic } = req.body;
 
     if (!pic) {
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { updateProfilePic } from "..//controllers/user.js";
+import { getUserDetails, updateProfilePic } from "..//controllers/user.js";
 import checkuser from "../middleware/checkuser.js";
 
 const router = express.Router();
 
+router.get("/getUserDetails", checkuser, getUserDetails);
 router.put("/updateProfilePic", checkuser, updateProfilePic);
 
 export default router;
